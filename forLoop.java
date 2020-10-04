5a83230f68803e29b98220a01ee1dd25a45e452e
Nay Hanif <71371234+NayHanif@users.noreply.github.com>
2020-10-04 17:26:15 +0700

    Add files via upload

--- forLoop.java
+++ forLoop.java
@@ -2,42 +2,17 @@ package com.pboreg;
 
 public class forLoop {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        System.out.println("Ini adalah asal program");
-
-        System.out.println("Loop pertama");
-
-        for (int nilai=5; nilai <= 10; nilai++){
-
-            System.out.println("For loop ke-" + nilai);
+        // Perulangan For
+        for(int i= 1; i<= 10; i = i+2){
+            System.out.printf(i + " ");
         }
-
-
-        System.out.println("Loop kedua");
-
-        for (int nilai=2; nilai < 10; nilai++){
-
-            System.out.println("For loop ke-" + nilai);
+        System.out.println("\n");
+        // Penggunaan For pada sebuah pemanggilan Array
+        String[] mobil = {"Volvo", "BMW", "Ford", "Mazda"};
+        for (String i : mobil){
+            System.out.println("Ini Mobil "+ i);
         }
-
-        System.out.println("Loop ketiga");
-
-        for (int nilai=10; nilai >= 5; nilai--){
-
-            System.out.println("For loop ke-" + nilai);
-        }
-
-        System.out.println("Loop keempat");
-
-        int nilai = 0;
-        for (; nilai < 10; ){
-
-            System.out.println("For loop ke-" + nilai);
-            nilai++;
-        }
-
-        System.out.println("Ini adalah akhir program");
-
     }
 }
---
+++ whileLoop.java
@@ -0,0 +1,14 @@
+package com.pboreg;
+
+public class whileLoop {
+
+    public static void main(String[] args){
+
+        int i = 0; // << ini inisialisasi
+        while (i < 5){
+            System.out.print(i + " ");
+            i++; //<< counter
+        }
+
+    }
+}
